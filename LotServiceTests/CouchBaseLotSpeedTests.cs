0aa01a8837594a5efff9c104137827abcd967dfa
Paul McCallick <paul@mccallick.com>
2014-04-22 10:21:34 -0700

    Added test to just get lots from cb

--- LotServiceTests/CouchBaseLotSpeedTests.cs
+++ LotServiceTests/CouchBaseLotSpeedTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LotContracts;
 using LotService;
 using NUnit.Framework;
 
@@ -18,10 +19,26 @@ namespace LotServiceTests
         }
 
         [Test]
-        public async void Get1000Lots100TimesByProtobuf()
+        public async void Get1000Lots100TimesViaHttpWithCouchbaseDataStore()
         {
-            LotRepository.UseCouch = true;
             await TestHelpers.GetPortfolioLots(100, true);
         }
+
+        [Test]
+        public void Get1000Lots100TimesViaCouchbase()
+        {
+            LotRepository repo;
+            List<Lot> lots;
+            for (int i = 0; i < 100; i++)
+            {
+                var start = DateTime.Now;
+                repo = new LotRepository();
+                lots = repo.GetLots().ToList();
+                Console.WriteLine(String.Format("time to get lots: {0}", (DateTime.Now - start).TotalMilliseconds));
+            }
+            
+        }
+
+
     }
 }
